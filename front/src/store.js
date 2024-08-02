bf98fdf32d030908f639a29b9d1620d93be84d76
SCM <hys1693359@gmail.com>
2024-08-02 15:52:53 +0900

    2024.08.02 : 기본 설정 추가

---
+++ front/src/store.js
@@ -0,0 +1,21 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { RouterProvider } from "react-router-dom";
+
+let user = createSlice({
+  name: "user",
+  initialState: [
+    { id: 1, name: "test-user1", age: 12 },
+    { id: 2, name: "test-user2", age: 13 },
+  ],
+  reducers: {
+    testFuc(state) {},
+  },
+});
+
+export let { testFuc } = user.actions;
+
+export default configureStore({
+  reducer: {
+    user: user.reducer,
+  },
+});
